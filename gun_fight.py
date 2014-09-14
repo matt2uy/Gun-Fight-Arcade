f429eb76974185ad2139411e62be2ca2463c838f
matt2uy <matt2uy@gmail.com>
2014-09-14 14:52:24 -0400

    Keep player within boundaries

--- gun_fight.py
+++ gun_fight.py
@@ -1,21 +1,25 @@
-# todo: simplify print_playing _field
+''' 
+todo: 
+- simplify print_playing _field
+- clear screen when refreshing playing field
+- Keep player within boundaries
+'''
 import sys
 import tty
-
 
 # keyboard input
 tty.setcbreak(sys.stdin)
 
 # game variables
 playing_field = [["-","-","-","-","-","-","-","-","-","-"],
-				 [" "," "," "," "," "," "," "," "," "," "],
-				 [" "," "," "," "," "," "," "," "," "," "],
-				 [" "," "," "," "," "," "," "," "," "," "],
-				 [" "," "," "," "," "," "," "," "," "," "],
-				 [" "," "," "," "," "," "," "," "," "," "],
-				 [" "," "," "," "," "," "," "," "," "," "],
-				 [" "," "," "," "," "," "," "," "," "," "],
-				 [" "," "," "," "," "," "," "," "," "," "],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
+				 ["|"," "," "," "," "," "," "," "," ","|"],
 				 ["-","-","-","-","-","-","-","-","-","-"]]
 
 game_over = False
@@ -25,18 +29,22 @@ def move_player(player, direction):
 	global player_one_x_location, player_one_y_location
 	if player == 1:
 		if direction == "up":
-			player_one_y_location-=1
+			if player_one_y_location != 0:	
+				player_one_y_location-=1
 		elif direction == "down":
-			player_one_y_location+=1
+			if player_one_y_location != 9:
+				player_one_y_location+=1
 		elif direction == "left":
-			player_one_x_location-=1
+			if player_one_x_location != 0:
+				player_one_x_location-=1
 		elif direction == "right":
-			player_one_x_location+=1
+			if player_one_x_location != 9:
+				player_one_x_location+=1
 	#elif player == 2:
 def update_playing_field():
 	playing_field[player_one_y_location][player_one_x_location] = 'p'
 def print_playing_field():
-	print '';
+	print "x: ", player_one_x_location, "y: ", player_one_y_location
 	for x in range(10):
 		print playing_field[x][0], playing_field[x][1], playing_field[x][2], playing_field[x][3], playing_field[x][4], playing_field[x][5], playing_field[x][6], playing_field[x][7], playing_field[x][8], playing_field[x][9]
 def game_loop():
